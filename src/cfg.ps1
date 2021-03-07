828a1c7cf2f93953f5faf141f521321dacb6ca35
acegene <acegene22@gmail.com>
2021-03-07 07:00:27 +0100

    M init/init.bash: update os parameter description; src/cfg.ps1: profile_path_2

--- src/cfg.ps1
+++ src/cfg.ps1
@@ -1,4 +1,5 @@
 $profile_path = "$($HOME)\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+$profile_path_2 = "$($HOME)\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 $editor = 'code'
 $bash = 'C:\Program Files\Git\bin\bash.exe'
 $perl = 'C:\Program Files\Git\usr\bin\perl.exe'
